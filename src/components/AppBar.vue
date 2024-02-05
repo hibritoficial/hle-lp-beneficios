6990348832e8365f878486e2b7cc8eba894f5815
Luan Mello <close8821@gmail.com>
2024-02-05 15:35:53 -0300

    Fix | Testando Nav-Bar

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -6,12 +6,12 @@
 
         <v-app-bar-title />
 
-        <div :class="smAndDown ? 'pt-1' : ''">
+        <div :class="smAndDown ? 'pt-2' : ''">
             <h6 class="text-quintenary font-weight-regular">Cadastro Gratuito</h6>
         </div>
 
         <div>
-            <v-btn :class="smAndDown ? 'mt-3' : ''" size="x-small" class="bg-primary text-quintenary ma-2" style="border-width: 1px; border-color: #555555;">
+            <v-btn :class="smAndDown ? 'mt-4' : ''" size="x-small" class="bg-primary text-quintenary ma-2" style="border-width: 1px; border-color: #555555;">
                 <h6>Acesso à Plataforma</h6>
             </v-btn>
         </div>
